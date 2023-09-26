1351e29b17a3e431771fed5436abdfe8312fe895
usuario <usuario@pc21-232>
2023-09-26 11:00:40 +0200

    MENSAJE ANGEL

--- src/HolaMundo.java
+++ src/HolaMundo.java
@@ -2,8 +2,8 @@
 public class HolaMundo {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("Hola CMEPPS!");
+		System.out.println("Bienvenido al curso 23/24!");
 	}
 
 }
